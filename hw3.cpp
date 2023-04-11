bf2c546056eefd9ce232c85b60d49a06cac21875
Abhilash Arnipalli <91401397+abhiarnipalli@users.noreply.github.com>
2023-04-10 20:17:24 -0500

    working code
    Working program

--- hw3.cpp
+++ hw3.cpp
@@ -1,71 +1,55 @@
 //
-// Created by Abhlash Arnipalli on 4/7/23.
+// Created by Abhilash Arnipalli on 4/7/23.
 //
-
-#include <iostream>
-#include <fstream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-// Helper function to compare two applicants based on WPM and IPM
-bool compareApplicants(const pair<float, float>& a, const pair<float, float>& b) {
-    if (a.first != b.first) {
-        return a.first > b.first; // Sort by decreasing WPM
-    } else {
-        return a.second < b.second; // Sort by increasing IPM if WPM is equal
+vector<int> recursiveBestApplicant(const vector<pair<float, float>>& applicant, int begin, int end) {
+    // Base case: there is only one applicant
+    if (begin == end) {
+        return {begin};
     }
-}
-
-// Recursive function to find the best applicants
-vector<int> recursiveBestApplicant(const vector<pair<float, float>>& applicants, int left, int right) {
-    vector<int> bestApplicants;
-
-    // Base case: If there is only one applicant, it is automatically the best
-    if (left == right) {
-        bestApplicants.push_back(left);
-        return bestApplicants;
-    }
-
-    // Divide the applicants into two groups
-    int mid = left + (right - left) / 2;
-    vector<int> bestLeft = recursiveBestApplicant(applicants, left, mid);
-    vector<int> bestRight = recursiveBestApplicant(applicants, mid + 1, right);
-
+    // Recursive case: divide the applicants in two groups
+    int middle = begin + (end - begin) / 2;
+    auto left = recursiveBestApplicant(applicant, begin, middle);
+    auto right = recursiveBestApplicant(applicant, middle+1, end);
     // Combine the results of the two sets to form the final solution
-    int bestIPM = INT_MAX;
-    for (int i : bestLeft) {
-        if (applicants[i].second < bestIPM) {
-            bestIPM = applicants[i].second;
-            bestApplicants.clear();
-            bestApplicants.push_back(i);
-        } else if (applicants[i].second == bestIPM) {
-            bestApplicants.push_back(i);
+    vector<int> best;
+    float best_wpm = 0.0, best_ipm = numeric_limits<float>::max();
+    int i = 0, j = 0;
+    while (i < left.size() || j < right.size()) {
+        if (i < left.size() && (j == right.size() || applicant[left[i]].first > applicant[right[j]].first)) {
+            // The applicant in the left group is better in WPM than the one in the right group
+            if (applicant[left[i]].second < best_ipm) {
+                // The applicant in the left group is also better in IPM
+                best_wpm = applicant[left[i]].first;
+                best_ipm = applicant[left[i]].second;
+                best = {left[i]};
+            } else if (applicant[left[i]].second == best_ipm) {
+                // The applicant in the left group is equally good in IPM as the best candidate so far
+                best.push_back(left[i]);
+            }
+            ++i;
+        } else {
+            // The applicant in the right group is better in WPM than the one in the left group
+            if (applicant[right[j]].second < best_ipm) {
+                // The applicant in the right group is also better in IPM
+                best_wpm = applicant[right[j]].first;
+                best_ipm = applicant[right[j]].second;
+                best = {right[j]};
+            } else if (applicant[right[j]].second == best_ipm) {
+                // The applicant in the right group is equally good in IPM as the best candidate so far
+                best.push_back(right[j]);
+            }
+            ++j;
         }
     }
-    for (int i : bestRight) {
-        if (applicants[i].second < bestIPM) {
-            bestIPM = applicants[i].second;
-            bestApplicants.clear();
-            bestApplicants.push_back(i);
-        } else if (applicants[i].second == bestIPM) {
-            bestApplicants.push_back(i);
-        }
-    }
-
-    // Sort the final solution by decreasing WPM
-    sort(bestApplicants.begin(), bestApplicants.end(), [&](int a, int b) {
-        return applicants[a].first > applicants[b].first;
-    });
-
-    return bestApplicants;
+    return best;
 }
 
-// Main function to find the best applicants
-vector<int> BestApplicants(const vector<pair<float, float>>& applicants) {
-    // Sort the applicants in decreasing order of WPM and increasing order of IPM
-    vector<pair<float, float>> sortedApplicants = applicants;
-    sort(sortedApplicants.begin(), sortedApplicants.end(), compareApplicants);
-
-    // Call the recursive function to find the best applicants
-    return recursiveBestApplicant(sortedApplicants, 0, sortedApplicants.size() - 1);
+vector<int> BestApplicants(const vector<pair<float, float>>& applicant) {
+    auto best = recursiveBestApplicant(applicant, 0, applicant.size() - 1);
+    sort(best.begin(), best.end());  // Sort the indices in ascending order
+    return best;
 }
--- hw3test.cpp
+++ hw3test.cpp
@@ -1,64 +1,56 @@
 //
-// Created by Abhlash Arnipalli on 4/7/23.
+// Created by Abhilash Arnipalli on 4/7/23.
 //
 #include <iostream>
 #include <fstream>
 #include <vector>
-
 using namespace std;
 
-vector<int> BestApplicants(const vector<pair<float, float> >& applicants);
+vector<int> BestApplicants(const vector<pair<float, float>>& applicants);
 
-vector<int> naiveBestApplicants(const vector<pair<float, float> >& applicants){
+vector<int> naiveBestApplicants(const vector<pair<float, float>>& applicants){
     vector<int> sol;
-
-    for (int i = 0; i < applicants.size(); i++)
-    {
+    for (int i = 0; i < applicants.size(); i++){
         bool eligible = true;
-        for (int j = 0; j < applicants.size(); j++)
-        {
-            if ((i != j) && (applicants[i].first < applicants[j].first) && (applicants[i].second > applicants[j].second))
-            {
+        for (int j = 0; j < applicants.size(); j++){
+            if ((i != j) && (applicants[i].first < applicants[j].first) && (applicants[i].second > applicants[j].second)){
                 eligible = false;
                 break;
             }
         }
-        if (eligible)
+        if (eligible) {
             sol.push_back(i);
+        }
     }
     return sol;
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
     int n;
-
-
-    ifstream ifile(argv[1]);
+    ifstream ifile("hw3data.txt");
     vector<pair<float, float> > applicants;
-
     ifile >> n;
 
     cout << "n : " << n << endl;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         float p, q;
         ifile >> p >> q;
         applicants.push_back(make_pair(p, q));
     }
-
     vector<int> sol = naiveBestApplicants(applicants);
 
-    cout << "Solution for Naive : ";
-    for (int i = 0; i < sol.size(); i++)
+    cout << "Naive Solution: ";
+    for (int i = 0; i < sol.size(); i++) {
         cout << sol[i] << " ";
+    }
     cout << endl;
 
     vector<int> sol2 = BestApplicants(applicants);
 
-    cout << "Solution for Recursive : ";
-    for (int i = 0; i < sol2.size(); i++)
+    cout << "Recursive Solution: ";
+    for (int i = 0; i < sol2.size(); i++) {
         cout << sol2[i] << " ";
+    }
     cout << endl;
 
     return 0;
